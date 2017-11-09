2cfd976dea891fc34599a5348f9dae94330f75b5
maitee2729 <lisa_hernandez27@hotmail.com>
2017-11-09 12:33:19 -0500

    Add files via upload

---
+++ Example4_Navbar_HTML_CSS_JS/scripts/navbar.js
@@ -0,0 +1,7 @@
+/* my js for the navbar to toggle it */
+
+function toggleNav(){
+    console.log("BLAH inside toggle Nav ...");
+    var nav = document.getElementById("nav");
+    nav.classList.toggle("navtoggle");
+}
